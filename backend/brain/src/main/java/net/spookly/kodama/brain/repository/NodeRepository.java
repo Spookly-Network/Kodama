a4c505172518aed54b53f6d805cf1a308725d76a
Niklas Ehlen <niklas.ehlen1@gmail.com>
2025-11-29 23:46:58 +0100

    arch(#8): add non-null annotations to `NodeRepository`
    Applied `@NonNull` annotations to enforce non-null constraints on `NodeRepository` and its parameters.

--- backend/brain/src/main/java/net/spookly/kodama/brain/repository/NodeRepository.java
+++ backend/brain/src/main/java/net/spookly/kodama/brain/repository/NodeRepository.java
@@ -1,12 +1,13 @@
 package net.spookly.kodama.brain.repository;
 
-import net.spookly.kodama.brain.domain.node.Node;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 import java.util.UUID;
 
-public interface NodeRepository extends JpaRepository<Node, UUID> {
+import lombok.NonNull;
+import net.spookly.kodama.brain.domain.node.Node;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NodeRepository extends JpaRepository<@NonNull Node, @NonNull UUID> {
 
     Optional<Node> findByName(String name);
 }
